e3010adc15a36187162b1d847b6272bcf5e7d662
Nizam Abdul <abdulnijamudeen@gmail.com>
2021-01-29 12:34:35 +0530

    Updated Home Work Save Functionality

--- src/app/teacher-portal/home-work/add/home-work-add.component.ts
+++ src/app/teacher-portal/home-work/add/home-work-add.component.ts
@@ -1,4 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { HomeWork } from 'src/app/model/HomeWork';
+import { HomeWorkService } from 'src/app/service/home-work.service';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-home-work-add',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeWorkAddComponent implements OnInit {
 
-  constructor() { }
+  homeWorkForm: HomeWork = {
+    id: 0,
+    title: '',
+    description: '',
+    date: new Date(),
+    grade: 1,
+    subject: ''
+  } as HomeWork;
+
+  homeWorks: Array<HomeWork> = [];
+
+  constructor(
+    private homeWorkService: HomeWorkService,
+    private location: Location
+    ) {
+    this.homeWorkService.homeWorks.subscribe((data) => { this.homeWorks = data; });
+  }
 
   ngOnInit(): void {
   }
+
+  onSaveClick = () => {
+    this.homeWorkForm.id = new Date().getTime();
+    this.homeWorks = [...this.homeWorks, this.homeWorkForm];
+    this.homeWorkService.update(this.homeWorks);
+    this.backClicked();
+  }
+
+  backClicked = () => this.location.back();
 
 }
